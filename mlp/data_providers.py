6fa4e655960b8c9029768069e7d5e6ad133fd44b
YasenPetrov <yasenpetr@gmail.com>
2017-10-02 12:54:52 +0100

    Complete lab 1

--- mlp/data_providers.py
+++ mlp/data_providers.py
@@ -133,11 +133,11 @@ class MNISTDataProvider(DataProvider):
         super(MNISTDataProvider, self).__init__(
             inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
 
-    # def next(self):
-    #    """Returns next data batch or raises `StopIteration` if at end."""
-    #    inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
-    #    return inputs_batch, self.to_one_of_k(targets_batch)
-    #
+    def next(self):
+       """Returns next data batch or raises `StopIteration` if at end."""
+       inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
+       return inputs_batch, self.to_one_of_k(targets_batch)
+
     def __next__(self):
         return self.next()
 
@@ -156,14 +156,19 @@ class MNISTDataProvider(DataProvider):
             to zero except for the column corresponding to the correct class
             which is equal to one.
         """
-        raise NotImplementedError()
+        unique_targets = np.sort(np.unique(int_targets))
+        result = np.zeros((len(int_targets), len(unique_targets)), dtype=np.int)
+        for i, row in enumerate(result):
+            row[np.where(unique_targets == int_targets[i])[0][0]] = 1
+        return result
+
 
 
 class MetOfficeDataProvider(DataProvider):
     """South Scotland Met Office weather data provider."""
 
     def __init__(self, window_size, batch_size=10, max_num_batches=-1,
-                 shuffle_order=True, rng=None):
+                 shuffle_order=True, rng=None, overlap_windows=False):
         """Create a new Met Offfice data provider object.
 
         Args:
@@ -188,19 +193,30 @@ class MetOfficeDataProvider(DataProvider):
             'Data file does not exist at expected path: ' + data_path
         )
         # load raw data from text file
-        # ...
+        data = np.loadtxt(data_path, skiprows=3)
         # filter out all missing datapoints and flatten to a vector
-        # ...
+        # Remove year and month columns
+        data = data[:, 2:]
+        data = data.flatten()
+        data = data[np.where(data != -99.99)]
         # normalise data to zero mean, unit standard deviation
-        # ...
+        data = (data - data.mean()) / data.std()
         # convert from flat sequence to windowed data
-        # ...
-        # inputs are first (window_size - 1) entries in windows
-        # inputs = ...
-        # targets are last entry in windows
-        # targets = ...
+        if not overlap_windows:
+            data = data[:len(data) - (len(data) % window_size)].reshape((int(len(data) / window_size), window_size))
+            # inputs are first (window_size - 1) entries in windows
+            inputs = data[:, :(window_size - 1)]
+            # targets are last entry in windows
+            targets = data[:, -1]
+        else:
+            window_count = len(data) - window_size + 1
+            inputs = np.empty((window_count, window_size - 1))
+            targets = np.empty((window_count,))
+            for i in range(window_count):
+                inputs[i] = data[i:i + window_size - 1]
+                targets[i] = data[i + window_size - 1]
         # initialise base class with inputs and targets arrays
-        # super(MetOfficeDataProvider, self).__init__(
-        #     inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
+        super(MetOfficeDataProvider, self).__init__(
+            inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
     def __next__(self):
             return self.next()
